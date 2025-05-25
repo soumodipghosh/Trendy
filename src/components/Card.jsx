c97f64224ac72085a8067d9b430736ae29a0b960
soumodip ghosh <ghosh.soumodip2002@gmail.com>
2025-05-25 12:21:14 +0530

    first coommit

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+
+const Card = ({ data }) => {
+  console.log(data);
+
+  const readMore = (url) => {
+    window.open(url);
+  }
+
+  if (!Array.isArray(data) || data.length === 0) {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="w-12 h-12 border-4 border-blue-400 border-dashed rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:justify-betwee md:ml-4'>
+      {data.map((curItem, index) => {
+        if (!curItem.urlToImage) {
+          return null;
+        } else {
+          return (
+          <div key={index} className='w-[330px] shadow bg-amber-200 rounded-2xl ml-5.5 lg:-ml-2 mt-5 border-s-amber-100 tracking-wide'>
+          <img src={curItem.urlToImage} alt="" className='w-[100%] h-[180px] border-l- border-r- rounded-lg' />
+          <div className='p-5'>
+            <a className='font-bold text-xl mt-1 hover:underline cursor-pointer' onClick={() => readMore(curItem.url)}>{curItem.title}</a>
+            <p className='mt-2 font-semibold'>{curItem.description}</p>
+            <button className='bg-blue-400 border-none cursor-pointer p-2 text-white text-lg rounded-lg mt-4 font-bold' onClick={() => readMore(curItem.url)}>Read More</button>
+          </div>
+        </div>
+          )
+        }
+
+})}
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/News.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
+
+const News = () => {
+  const [search, setSearch] = useState("india");
+  const [newsData, setNewsData] = useState([])
+  
+
+  const API_KEY = "deead443f0014fa3bd4301636136f0f8";
+
+  const getData = async() => {
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+    const jsonData = await response.json();
+    console.log(jsonData.articles);
+    setNewsData(jsonData.articles)
+  }
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value)
+  }
+  const userInput = (e) => {
+    setSearch(e.target.value)
+  }
+  useEffect(() => {
+    getData()
+  }, [search])
+
+
+
+  return (
+    <div>
+    
+      <nav className='flex items-center justify-around bg-blue-200 p-2 lg:p-2 gap-16 font-semibold fixed top-0 z-50 w-full'>
+        <div className='bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white p-2 rounded-lg font-mono'>
+            <h1>Trendy</h1>
+        </div>
+        <ul className='hidden lg:flex md:flex lg:gap-10 md:gap-5'>
+            <li href="" onClick={() => setSearch("All News")} className='hover:underline cursor-pointer'>All News</li>
+            <li href="" onClick={() => setSearch("Trending")} className='hover:underline cursor-pointer'>Trending</li>
+        </ul>
+        <div className='flex gap-1 items-center'>
+            <input type="text" placeholder='Search Here...' value={search} className='w-[150px] border-none p-1 rounded-lg' onChange={handleInput} />
+            <button className='bg-rose-400 p-1.5 md:px-3 rounded-lg text-white cursor-pointer' onClick={getData}>Search</button>
+        </div>
+      </nav>
+    
+  <div className="w-full overflow-hidden bg-yellow-200 mt-14">
+  <div
+    className="whitespace-nowrap inline-block animate-[scroll_15s_linear_infinite] font-semibold text-blue-800 text-sm sm:text-base md:text-lg px-4 py-2"
+    style={{
+      display: 'inline-block',
+      whiteSpace: 'nowrap',
+      animation: 'scroll 20s linear infinite',
+    }}
+  >
+    🔥 Breaking News : India launches new satellite | 🌍 Global Markets Rally | 🏏 Cricket World Cup Highlights | ⚽ Football World Cup highlights | 🛠️ Tech Giant Unveils New AI Tools | 💡 Stay Tuned !
+  </div>
+
+  <style>
+    {`
+      @keyframes scroll {
+        0% { transform: translateX(100%); }
+        100% { transform: translateX(-100%); }
+      }
+    `}
+  </style>
+</div>
+
+
+
+
+
+      <div className='justify-center gap-2 grid grid-cols-3 lg:flex p-1 lg:gap-5 font-semibold text-white mt-3'>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Sports">Sports</button>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Politics">Politics</button>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Entertainment">Entertain</button>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Health">Health</button>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Fitness">Fitness</button>
+        <button className='bg-blue-400 p-1 rounded-xl cursor-pointer lg:px-6 lg:py-1' onClick={userInput} value="Technology">Tech</button>
+      </div> 
+
+      <div>
+        {newsData ? <Card data={newsData}/> : [] }
+      </div>
+    </div>
+  )
+}
+
+export default News
